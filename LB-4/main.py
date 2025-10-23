fa87f8582d81c50f858e2812633236a329c3b339
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-10-23 09:39:19 +0300

    Fix errors

--- LB-4/main.py
+++ LB-4/main.py
@@ -1,9 +1,8 @@
-import CsvReader
-import DataPreprocessor
-import DataAnalyzer
-import DataUploader
-import DatabaseQueries
-
+from CsvReader import CsvReader
+from DataPreprocessor import DataPreprocessor
+from DataAnalyzer import DataAnalyzer
+from DataUploader import DataUploader
+from DatabaseQueries import DatabaseQueries
 csv_url = "https://informer.com.ua/dut/python/import/129-indeksi-tsin-na-zhitlo.csv"
 
 csv_reader = CsvReader()
@@ -12,17 +11,13 @@ data = csv_reader.read_data(csv_url)
 preprocessor = DataPreprocessor(data)
 processed_data = preprocessor.add_index_ids()
 
-
 analyzer = DataAnalyzer(data)
-
 
 indices = analyzer.get_indices()
 print("Індекси:", indices)
 
-
 periods = analyzer.get_periods()
 print("Періоди:", periods)
-
 
 indices_by_period = analyzer.get_indices_by_period()
 print("Індекси за періодами:", indices_by_period)
@@ -43,3 +38,6 @@ average_indices = queries.get_average_index_by_period()
 print("Середні значення індексів за періодами:", average_indices)
 
 queries.close()
+
+
+
