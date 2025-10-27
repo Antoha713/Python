a272bf62147b65f7e77c1ebd0cb1838c18d06ff1
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-10-27 18:49:40 +0200

    Update studentData.py

--- SR-4/studentData.py
+++ SR-4/studentData.py
@@ -7,3 +7,16 @@ class StudentData:
         self.student = student
         self.real_perf = real_perf
         self.desired_perf = desired_perf
+
+    def get_data(self):
+        return {
+            "PIB": self.student.get_pib(),
+            "Group": self.student.get_group_number(),
+            "BirthDate": self.student.get_birth_date(),
+            "Address": self.student.get_address(),
+            "Subjects": self.real_perf.subjects,
+            "RealGrades": self.real_perf.grades,
+            "RealAvg": self.real_perf.average_grade(),
+            "DesiredGrades": self.desired_perf.grades,
+            "DesiredAvg": self.desired_perf.average_grade()
+        }
