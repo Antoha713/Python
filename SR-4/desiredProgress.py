873c14febc6cd56bad2c99cee68e0bb7d6a3a9c8
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-10-27 18:48:16 +0200

    Add metod average_grade

--- SR-4/desiredProgress.py
+++ SR-4/desiredProgress.py
@@ -4,3 +4,9 @@ class DesiredProgress(Progress):
     def __init__(self, subjects: list, grades: list, desired_avg: float = None):
         super().__init__(subjects, grades)
         self.desired_avg = desired_avg
+
+    def average_grade(self):
+
+        if self.desired_avg is not None:
+            return self.desired_avg
+        return sum(self.grades) / len(self.grades)
