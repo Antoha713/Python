eda7117096573d85f5401e8520dec24718909eb1
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-10-28 09:00:36 +0200

    Add method

--- PR-5/firstsite/firstapp/views.py
+++ PR-5/firstsite/firstapp/views.py
@@ -22,3 +22,25 @@ def popular_singers(request):
     """
     return HttpResponse(html_content, content_type='text/html; charset=utf-8')
 
+
+def singer_card(request):
+
+    singer_id = request.GET.get('id')
+
+    singer_id = int(singer_id)
+
+    singers = list_singers()
+
+    singer = next((s for s in singers if s['id'] == singer_id), None)
+
+
+    html_content = f"""
+    <html>
+    <body>
+        <h1>{singer['name']}</h1>
+        <p>Жанр: {singer['genre']}</p>
+        <p>Вокаліст: {singer['lead_singer']}</p>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_content, content_type='text/html; charset=utf-8')
