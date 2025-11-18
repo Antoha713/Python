5b48640b078dd2aa217ce391c85a7aaabfed71e0
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-18 09:04:34 +0200

    create class BloggersData

---
+++ SR-6/firstsite/firstapp/data.py
@@ -0,0 +1,17 @@
+class BloggersData:
+    bloggers = {
+        1: {
+            "name": "bloger1",
+            "category": "Технології",
+            "description": "Огляди гаджетів",
+            "social": "https://youtube.com/bloger1",
+            "posts": ["Огляд iPhone 16", "Найкращі ноутбуки 2025"],
+        },
+        2: {
+            "name": "bloger2",
+            "category": "Кулінарія",
+            "description": "Рецепти та огляди кухонь світу",
+            "social": "https://instagram.com/bloger2",
+            "posts": ["5 страв за 10 хв", "Італійська кухня"],
+        },
+    }
