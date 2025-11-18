8cf77448be04f109a1dbea77b177540e05f04079
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-18 09:19:19 +0200

    create new route

--- SR-6/firstsite/firstapp/urls.py
+++ SR-6/firstsite/firstapp/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('profiles/', views.profiles, name='profiles'),
     path('news/', views.redirect_to_home, name='news'),
+    path('news-page/', views.news_page, name='news_page'),
 
     path('profiles/<int:blogger_id>/', views.profile_details, name='profile_details')
 ]
