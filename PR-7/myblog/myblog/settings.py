f25f112c8926d1822fac3ba4cf5fd33ab73e3f74
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-20 09:19:13 +0200

    add settings

--- PR-7/myblog/myblog/settings.py
+++ PR-7/myblog/myblog/settings.py
@@ -40,6 +40,13 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
