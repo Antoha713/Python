1624fe974b49f77c2c4e29c79258fe6e3d091d94
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-20 08:38:45 +0200

    add data to database

--- PR-7/duikt_exam_zhylyk/duikt_task_zhylyk/views.py
+++ PR-7/duikt_exam_zhylyk/duikt_task_zhylyk/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import cars_brand, cars_info
 
-# Create your views here.
+
+def install(request):
+    output = []
+
+    brands = [
+        {"BRAND_NAME": "Toyota", "BRAND_COUNTRY": "Japan", "BRAND_RATING": 9},
+        {"BRAND_NAME": "Ford", "BRAND_COUNTRY": "USA", "BRAND_RATING": 8},
+        {"BRAND_NAME": "BMW", "BRAND_COUNTRY": "Germany", "BRAND_RATING": 9},
+    ]
+
+    cars = [
+        {"CAR_NAME": "Corolla", "CAR_MODEL": "2023", "CAR_PRICE": 20000, "CAR_BRAND": "Toyota"},
+        {"CAR_NAME": "Mustang", "CAR_MODEL": "2022", "CAR_PRICE": 35000, "CAR_BRAND": "Ford"},
+        {"CAR_NAME": "X5", "CAR_MODEL": "2023", "CAR_PRICE": 60000, "CAR_BRAND": "BMW"},
+    ]
+
+
+
+    return HttpResponse("<br>".join(output))
