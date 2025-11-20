25324cfbcb3a3c234c083e8b3236c0f7d7101da6
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-20 09:08:26 +0200

    update models

--- PR-7/myblog/blog/models.py
+++ PR-7/myblog/blog/models.py
@@ -16,10 +16,10 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    publish = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
