a508c3d1ca8bbb0a977bd4c5ef4c61e8d7353371
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-20 09:07:04 +0200

    update html

---
+++ SR-7/duikt_exam_zhylyk/duikt_task_zhylyk/views.py
@@ -0,0 +1,53 @@
+from django.http import HttpResponse
+from .models import cars_brand, cars_info
+
+from django.shortcuts import render
+
+def show_cars(request):
+    all_cars = cars_info.objects.all()
+    return render(request, "cars.html", {"cars": all_cars})
+
+def install(request):
+    output = []
+
+    brands = [
+        {"BRAND_NAME": "Toyota", "BRAND_COUNTRY": "Japan", "BRAND_RATING": 9},
+        {"BRAND_NAME": "Ford", "BRAND_COUNTRY": "USA", "BRAND_RATING": 8},
+        {"BRAND_NAME": "BMW", "BRAND_COUNTRY": "Germany", "BRAND_RATING": 9},
+    ]
+
+    cars = [
+        {"CAR_NAME": "Corolla", "CAR_MODEL": "2023", "CAR_PRICE": 20000, "CAR_BRAND": "Toyota"},
+        {"CAR_NAME": "Mustang", "CAR_MODEL": "2022", "CAR_PRICE": 35000, "CAR_BRAND": "Ford"},
+        {"CAR_NAME": "X5", "CAR_MODEL": "2023", "CAR_PRICE": 60000, "CAR_BRAND": "BMW"},
+    ]
+
+    for b in brands:
+        obj, created = cars_brand.objects.get_or_create(
+            BRAND_NAME=b["BRAND_NAME"],
+            defaults={
+                "BRAND_COUNTRY": b["BRAND_COUNTRY"],
+                "BRAND_RATING": b["BRAND_RATING"]
+            }
+        )
+        if created:
+            output.append(f"Бренд додано: {b['BRAND_NAME']}")
+        else:
+            output.append(f"Бренд пропущено (вже існує): {b['BRAND_NAME']}")
+
+    for c in cars:
+        brand = cars_brand.objects.get(BRAND_NAME=c["CAR_BRAND"])
+        obj, created = cars_info.objects.get_or_create(
+            CAR_NAME=c["CAR_NAME"],
+            CAR_MODEL=c["CAR_MODEL"],
+            defaults={
+                "CAR_PRICE": c["CAR_PRICE"],
+                "CAR_BRAND": brand
+            }
+        )
+        if created:
+            output.append(f"Авто додано: {c['CAR_NAME']}")
+        else:
+            output.append(f"Авто пропущено (вже існує): {c['CAR_NAME']}")
+
+    return HttpResponse("<br>".join(output))
