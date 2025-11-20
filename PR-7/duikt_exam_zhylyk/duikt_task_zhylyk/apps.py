19cd710aaab973036a0259590dc380f37932f944
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-20 09:05:08 +0200

    create url

--- PR-7/duikt_exam_zhylyk/duikt_task_zhylyk/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DuiktTaskZhylykConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'duikt_task_zhylyk'
--- PR-7/duikt_exam_zhylyk/duikt_task_zhylyk/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import install, show_cars
-
-urlpatterns = [
-    path('install/', install),
-    path('duikt_page_zhylyk/', show_cars),
-]
