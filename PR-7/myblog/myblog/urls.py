772e27a24bc4f4ca39e3c6c715f44f06ce0dfb02
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-20 09:11:17 +0200

    change views

--- PR-7/myblog/myblog/urls.py
+++ PR-7/myblog/myblog/urls.py
@@ -1,7 +1,14 @@
+from django import views
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'blog'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')), 
+    path('', include('blog.urls')),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
+
 ]
