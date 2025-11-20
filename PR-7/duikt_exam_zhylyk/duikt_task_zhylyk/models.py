d52a7cf9aa41e967251cb69ea0d8f8f91e19b19e
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-20 08:34:56 +0200

    add class brand

--- PR-7/duikt_exam_zhylyk/duikt_task_zhylyk/models.py
+++ PR-7/duikt_exam_zhylyk/duikt_task_zhylyk/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class cars_brand(models.Model):
+    BRAND_NAME = models.CharField(max_length=100)
+    BRAND_COUNTRY = models.CharField(max_length=100)
+    BRAND_RATING = models.IntegerField()
+
+    def __str__(self):
+        return self.BRAND_NAME
+
