0f02b42e2e2bb066ec40a944ddd0a184f457f9e0
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-25 06:18:27 +0200

    add comments and finish project

--- PR-7.1/myblog/blog/views.py
+++ PR-7.1/myblog/blog/views.py
@@ -1,7 +1,20 @@
-from .models import Post
+from .models import Post, Comment
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
+from .forms import CommentForm
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -61,5 +74,10 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
+    return render(request, 'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
