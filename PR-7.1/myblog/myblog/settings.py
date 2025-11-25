b5398adf24a5b344497842287f470756f1ff3dbc
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-11-25 05:38:23 +0200

    change views.py and add emails

--- PR-7.1/myblog/myblog/settings.py
+++ PR-7.1/myblog/myblog/settings.py
@@ -99,6 +99,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
