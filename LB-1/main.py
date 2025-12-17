dd817ad95bcf355a669431611264fba737c71c14
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-12-17 21:53:33 +0200

    task 2

--- LB-1/main.py
+++ LB-1/main.py
@@ -1,3 +1,4 @@
+#1
 user_input = input("Будь ласка, введіть список чисел, розділених комами: ")
 numbers_list = [int(item) for item in user_input.split(',')]
 numbers_list.sort()
@@ -8,3 +9,15 @@ else:
     median = (numbers_list[n // 2 - 1] + numbers_list[n // 2]) / 2
 
 print(f"Медіана: {median}")
+
+#2
+books = {}
+while True:
+    book_input = input("Введіть назву книги та автора, розділені комою (або 'stop' для завершення): ")
+    if book_input.lower() == 'stop':
+        break
+    try:
+        book, author = book_input.split(',', 1)  # Розділяємо введення на назву книги та автора
+        books[book.strip()] = author.strip()  # Видаляємо зайві пробіли та зберігаємо дані
+    except ValueError:
+        print("Некоректний ввід, спробуйте ще раз.")
