2cd61df4d3d7fd5f0e7d93797582cf2b65563f5b
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-12-17 21:50:50 +0200

    count mediana

--- LB-1/main.py
+++ LB-1/main.py
@@ -1,3 +1,8 @@
 user_input = input("Будь ласка, введіть список чисел, розділених комами: ")
 numbers_list = [int(item) for item in user_input.split(',')]
 numbers_list.sort()
+n = len(numbers_list)
+if n % 2 == 1:
+    median = numbers_list[n // 2]
+else:
+    median = (numbers_list[n // 2 - 1] + numbers_list[n // 2]) / 2
