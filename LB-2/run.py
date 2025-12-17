bd8d0cfdf9b71b596336a27945405dd29a492693
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-12-18 00:41:12 +0200

    update run

--- LB-2/run.py
+++ LB-2/run.py
@@ -1,12 +1,12 @@
 from FullTimeStudent import FullTimeStudent
+from PartTimeStudent import PartTimeStudent
 
-student1 = FullTimeStudent(name="KOLA", age=24,
-                           prac_score=910, prac_count=10,
-                           exam_scr=95, attend_pct=85)
+student1 = FullTimeStudent(name="KOLA", age=24, prac_score=910, prac_count=10, exam_scr=95, attend_pct=85)
+student2 = FullTimeStudent("Klavdia Petrivna", 19, 930, 10, 99, 20)
+student3 = PartTimeStudent("CHEEV", 22, 390, 4, 78)
+student4 = PartTimeStudent("YAKTAK", 21, 480, 5, 82)
 
-student2 = FullTimeStudent(name="Klavdia Petrivna", age=19,
-                           prac_score=930, prac_count=10,
-                           exam_scr=99, attend_pct=20)
+university_students = [student1, student2, student3, student4]
 
 print("Студент 1:")
 print("Ім'я:", student1.name)
@@ -19,3 +19,11 @@ print("Ім'я:", student2.name)
 print("Вік:", student2.age)
 print("Середній бал за практичні:", student2.avg_practice_score())
 print("Загальний бал:", round(student2.total_score(), 1))
+
+for student in university_students:
+    student.display_info()
+    if isinstance(student, FullTimeStudent):
+        print(f"Загальний бал: {student.total_score()}")
+    elif isinstance(student, PartTimeStudent):
+        print(f"Загальний бал (заочно): {student.total_score()}")
+    print("---------")
