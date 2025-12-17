80c2701a07e9c0f2bfdce9a9a4d641cf2a4d70af
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-12-18 00:41:24 +0200

    fix errors

--- LB-2/FullTimeStudent.py
+++ LB-2/FullTimeStudent.py
@@ -3,11 +3,8 @@ from Person import Person
 class FullTimeStudent(Person):
 
     def __init__(self, name, age, prac_score, prac_count, exam_scr, attend_pct):
-        super().__init__(name, age)
-        self.prac_score = prac_score       # сумарний_бал_за_практичні
-        self.prac_count = prac_count       # кількість_практичних
-        self.exam_scr = exam_scr           # бал_за_іспит
-        self.attend_pct = attend_pct     # процент_відвідування
+        super().__init__(name, age, prac_score, prac_count, exam_scr)
+        self.attend_pct = attend_pct
 
     def avg_practice_score(self):
         if self.prac_count != 0:
--- LB-2/PartTimeStudent.py
+++ LB-2/PartTimeStudent.py
@@ -3,10 +3,7 @@ from Person import Person
 class PartTimeStudent(Person):
 
     def __init__(self, name, age, prac_score, prac_count, exam_scr):
-        super().__init__(name, age)
-        self.prac_score = prac_score       # сумарний_бал_за_практичні
-        self.prac_count = prac_count       # кількість_практичних
-        self.exam_scr = exam_scr           # бал_за_іспит
+        super().__init__(name, age, prac_score, prac_count, exam_scr)
 
     def avg_practice_score(self):
         if self.prac_count != 0:
