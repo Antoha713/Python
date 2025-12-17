3f0cf01dc7fbd65282a49df294ab88c34c042f51
Antoha713 <a.zhylyk@universum.kiev.ua>
2025-12-17 22:27:01 +0200

    calculate all salary function

--- SR-2/salary.py
+++ SR-2/salary.py
@@ -1,3 +1,13 @@
 def calculate_salary(salary, days):
     return (salary / 30) * days
 
+
+def calculate_all_salaries(employees):
+    result = {}
+
+    for name in employees:
+        salary = employees[name]["salary"]
+        days = employees[name]["days"]
+        result[name] = round(calculate_salary(salary, days), 2)
+
+    return result
